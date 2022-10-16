47104bed747d2da06e94e19a8e4279d4fb46749d
Daniel Khater <formaldk737@gmail.com>
2022-10-16 11:12:47 +0300

    Small Tweaks and Additions
    I didn't have much time to work today, so I just made some small tweaks: I added a Level complete message and added fog and some stuff to the environment.

--- FPS Game Project/Assets/Scripts/Shoot.cs
+++ FPS Game Project/Assets/Scripts/Shoot.cs
@@ -14,7 +14,7 @@ public class Shoot : MonoBehaviour
         float bullet_origin_z = origin_point.transform.position.z;
         if (Input.GetMouseButtonDown(0)){
             GameObject bullet = Instantiate(bullet_prefab, new Vector3 (bullet_origin_x, bullet_origin_y, bullet_origin_z), Quaternion.identity);
-            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 500000 * Time.deltaTime);
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 455599 * Time.deltaTime);
         }
     }
 }
--- FPS Game Project/Assets/Scripts/TextManager.cs
+++ FPS Game Project/Assets/Scripts/TextManager.cs
@@ -10,9 +10,13 @@ public class TextManager : MonoBehaviour
     Text scoreText;
 
     [SerializeField]
-    Text eastereggText;
+    Text targetsLeftText;
+
+    // [SerializeField]
+    // Text eastereggText;
     
     int score = 0;
+    int targets_left = 1;
 
     private void Awake() {
         instance = this;
@@ -22,18 +26,30 @@ public class TextManager : MonoBehaviour
     {
         //highScore = PlayerPrefs.GetInt("highScore",0);
         scoreText.text = "Score: " + score.ToString();
+        targetsLeftText.text = "Targets Left: " + targets_left.ToString();
     }
 
     public void AddPoint(){
         score += 20;
+        targets_left --;
         scoreText.text = "Score: " + score.ToString();
+        targetsLeftText.text = "Targets Left: " + targets_left.ToString();
+        if (targets_left == 0){
+            GameObject congrats = GameObject.Find("Level Complete");
+            GameObject verticalCrosshair = GameObject.Find("Vertical");
+            GameObject horizontalCrosshair = GameObject.Find("Horizontal");
+            verticalCrosshair.GetComponent<Image>().enabled = false;
+            horizontalCrosshair.GetComponent<Image>().enabled = false;
+            congrats.GetComponent<Text>().text = "Level Complete";
+        }
         // if (highScore < score){
         //     PlayerPrefs.SetInt("highScore", score);
         // } I plan on implemening a sifferent way to keep a high score later
     }
     
     public void DiplayCongrats(){
-        eastereggText.text = "Easter Egg Found!";
+        GameObject eastereggText = GameObject.Find("Easter Egg Notification");
+        eastereggText.GetComponent<Text>().text = "Easter Egg Found!";
         score += 50;
         scoreText.text = "Score: " + score.ToString();
     }
